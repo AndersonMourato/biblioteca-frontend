3d1fd7ace5c2a49bd6599a29681ee5e74a4e10ae
Anderson Inacio <anderson.mouratoo@gmail.com>
2024-07-13 23:52:40 -0300

    init

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/biblioteca-font-end'),
+      dir: require('path').join(__dirname, './coverage/biblioteca-front-end'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'biblioteca-font-end'`, () => {
+  it(`should have as title 'biblioteca-front-end'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('biblioteca-font-end');
+    expect(app.title).toEqual('biblioteca-front-end');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('biblioteca-font-end app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('biblioteca-front-end app is running!');
   });
 });
